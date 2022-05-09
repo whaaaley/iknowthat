f98991aafcb314a22fe2a2f90f9d0a21dcf3ea83
Whaley <dustindowell22@gmail.com>
2022-05-09 11:53:11 -0500

    Select active message

--- react-dating/src/features/messages/index.js
+++ react-dating/src/features/messages/index.js
@@ -1,22 +1,34 @@
 
 import { useState } from 'react'
 import { useSelector } from 'react-redux'
+import cc from 'classcat'
 
 import ShadowRoot from '$/modules/ShadowRoot.js'
 import style from 'bundle-text:./_messages.scss'
 
 export default function () {
+  const [state, setState] = useState({ index: 0 })
   const messages = useSelector(state => state.messages.data)
+
+  const setIndex = index => () => setState({ index })
 
   return <ShadowRoot id='messages' styles={[style]}>
     {messages.map((message, index) => {
+      const className = cc({
+        'item': true,
+        '-active': index === state.index
+      })
+
       const variables = {
         '--photo': `url(${message.photo})`
       }
 
-      return <div className='item' style={variables} key={index}>
-        {/* nothing yet... */}
-        <h1>hi</h1>
+      return <div className={className} style={variables} key={index} onClick={setIndex(index)}>
+        <div className='photo'></div>
+        <div>
+          <div className='name'>{message.name}</div>
+          <div className='lastMessage'>{message.lastMessage}</div>
+        </div>
       </div>
     })}
   </ShadowRoot>
